5568ad68c081f8bf961a5720487e0b3b915e73a9
Amrut Deshpande <amrutdeshpande2004@gmail.com>
2026-01-03 11:55:33 +0530

    .

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -51,7 +51,7 @@ export const register = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax',
+            sameSite: 'None',
             maxAge: 24 * 60 * 60 * 1000
         })
 
@@ -122,7 +122,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000
     })
 
